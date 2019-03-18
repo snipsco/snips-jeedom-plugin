08a26f22cc3798a29d644e5172a00c536c57de20
CoorFun <coorfang@gmail.com>
2019-03-18 19:09:23 +0100

    [Add] client id generator

--- core/class/snips.utils.php
+++ core/class/snips.utils.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Snips\utils;
+namespace SnipsUtils;
 
 function get_intents_from_assistant_json($path)
 {
@@ -21,4 +21,21 @@ function get_intents_from_assistant_json($path)
         }
     }
     return json_encode($intents_slots);
+}
+
+function generate_client_id()
+{
+    $chars = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y','z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+    
+    $keys =array_rand($chars, 10);
+    $client_id ='';
+    for ($i = 0; $i < 10; $i++) {
+        $client_id .= $chars[$keys[$i]];
+    }
+    return $client_id;
 }
