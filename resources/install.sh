080eab58ac3f3bf2690a17b78ed6967c7efe8916
CoorFun <coorfang@gmail.com>
2019-03-19 11:19:08 +0100

    [Fix] restart apache2 at the end of all installation

--- resources/install.sh
+++ resources/install.sh
@@ -1,12 +1,33 @@
 #! /bin/bash
+PROGRESS_FILE=/tmp/jeedom/snips/dependance
+echo "[jeedom-plugin-snips]"
+echo "--------------------------------"
+echo "[*] Start to install dependencies."
 
-echo "Start to install dependances"
+touch ${PROGRESS_FILE}
+echo 0 > ${PROGRESS_FILE}
 
-touch /tmp/snips_dep
-echo 0 > /tmp/snips_dep
+echo "--------------------------------"
+echo "[*] Checking necessary working directories."
+if [ ! -d "../config_running" ]; then
+    echo "[*] Creating config_running folder.."
+    mkdir ../config_running
+fi
+
+if [ ! -d "../config_backup" ]; then
+    echo "[*] Creating config_backup folder.."
+    mkdir ../config_backup
+fi
+echo 5 > ${PROGRESS_FILE}
+
+echo "--------------------------------"
+echo "[*] Installing lsb-release, php-pear"
 apt-get -y install lsb-release php-pear
-archi=`lscpu | grep Architecture | awk '{ print $2 }'`
+echo 10 > ${PROGRESS_FILE}
 
+echo "--------------------------------"
+echo "[*] Checking system architecture.."
+archi=`lscpu | grep Architecture | awk '{ print $2 }'`
 if [ "$archi" == "x86_64" ]; then
     if [ `lsb_release -i -s` == "Debian" ]; then
         wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
@@ -26,22 +47,26 @@ if [ "$archi" == "x86_64" ]; then
         fi
     fi
 fi
+echo 20 > ${PROGRESS_FILE}
 
-echo 10 > /tmp/snips_dep
-
+echo "--------------------------------"
+echo "[*] Updating package list.."
 apt-get update
-echo 30 > /tmp/snips_dep
-echo "Installing mqtt dependances"
+echo 30 > ${PROGRESS_FILE}
+
+echo "--------------------------------"
+echo "[*] Installing mqtt dependencies.."
 apt-get -y install mosquitto mosquitto-clients libmosquitto-dev
+echo 50 > ${PROGRESS_FILE}
 
-echo 80 > /tmp/snips_dep
-
+echo "--------------------------------"
+echo "[*] Checking PHP version.."
 if [[ -d "/etc/php5/" ]]; then
-    echo "PHP5 is detected"
+    echo "[*] PHP5 is detected"
+    echo "[*] Configuring PHP5 extension.."
     apt-get -y install php5-dev
     if [[ -d "/etc/php5/cli/" && ! `cat /etc/php5/cli/php.ini | grep "mosquitto"` ]]; then
         echo "" | pecl install Mosquitto-alpha
-        echo 80 > /tmp/snips_dep
         echo "extension=mosquitto.so" | tee -a /etc/php5/cli/php.ini
     fi
     if [[ -d "/etc/php5/fpm/" && ! `cat /etc/php5/fpm/php.ini | grep "mosquitto"` ]]; then
@@ -50,29 +75,27 @@ if [[ -d "/etc/php5/" ]]; then
     fi
     if [[ -d "/etc/php5/apache2/" && ! `cat /etc/php5/apache2/php.ini | grep "mosquitto"` ]]; then
         echo "extension=mosquitto.so" | tee -a /etc/php5/apache2/php.ini
-        service apache2 restart
     fi
-    echo "PHP5 Mosquitto has been installed"
+    echo "[*] PHP5 Mosquitto has been installed"
 fi
+echo 80 > ${PROGRESS_FILE}
 
 if [[ -d "/etc/php/7.0" ]]; then
-    echo "PHP7 is detected"
+    echo "[*] PHP7.0 is detected"
+    echo "[*] Configuring PHP7.0 extension.."
     apt-get -y install php7.0-dev
     if [[ -d "/etc/php/7.0/cli/" && ! `cat /etc/php/7.0/cli/php.ini | grep "mosquitto"` ]]; then
         echo "" | pecl install Mosquitto-alpha
-        echo 80 > /tmp/snips_dep
         echo "extension=mosquitto.so" | tee -a /etc/php/7.0/cli/php.ini
     fi
     if [[ -d "/etc/php/7.0/apache2/" && ! `cat /etc/php/7.0/apache2/php.ini | grep "mosquitto"` ]]; then
         echo "extension=mosquitto.so" | tee -a /etc/php/7.0/apache2/php.ini
-        service apache2 restart
     fi
-    echo "PHP7 Mosquitto has been installed"
+    echo "[*] PHP7 Mosquitto has been installed"
 fi
+echo 100 > ${PROGRESS_FILE}
 
-rm /tmp/snips_dep
-
-mkdir ../config_running
-mkdir ../config_backup
-
-echo "Dependances installation is done"
+echo "--------------------------------"
+echo "[*] Installation is done"
+rm ${PROGRESS_FILE}
+sudo service apache2 restart
--- resources/remove.sh
+++ resources/remove.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+echo "[jeedom-plugin-snips]"
+echo "--------------------------------"
+echo "[*] Start to remove dependencies."
+
 if [[ -d "/etc/php5/" ]]; then
     if [[ -d "/etc/php5/cli/" && ! `cat /etc/php5/cli/php.ini | grep "mosquitto"` ]]; then
         sed -i '/extension=mosquitto.so/d' /etc/php5/cli/php.ini
@@ -13,16 +17,15 @@ if [[ -d "/etc/php5/" ]]; then
         service apache2 restart
     fi
 fi
-if [[ -d "/etc/php7/" ]]; then
+if [[ -d "/etc/php/7.0/" ]]; then
     if [[ -d "/etc/php/7.0/cli/" && `cat /etc/php/7.0/cli/php.ini | grep "mosquitto"` ]]; then
         sed -i '/extension=mosquitto.so/d' /etc/php/7.0/cli/php.ini
-    fi
-    if [[ -d "/etc/php/7.0/fpm/" && `cat /etc/php/7.0/fpm/php.ini | grep "mosquitto"` ]]; then
-        sed -i '/extension=mosquitto.so/d' /etc/php/7.0/fpm/php.ini
-        service php5-fpm restart
     fi
     if [[ -d "/etc/php/7.0/apache2/" && `cat /etc/php/7.0/apache2/php.ini | grep "mosquitto"` ]]; then
         sed -i '/extension=mosquitto.so/d' /etc/php/7.0/apache2/php.ini
         service apache2 restart
     fi
 fi
+
+echo "--------------------------------"
+echo "[*] Uninstallation is done."
